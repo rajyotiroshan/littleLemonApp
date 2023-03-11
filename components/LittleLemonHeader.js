5905b0d3d07b142f0505c6e3e9d4e9be6ec9ea6c
rajan <rajyotiroshan@gmail.com>
2023-03-11 08:48:48 +0530

    header

---
+++ components/LittleLemonHeader.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { Text, View } from "react-native";
+
+export default function LittleLemonHeader() {
+    return (
+      <View style={{ flex: 0.15, backgroundColor: '#F4CE14' }}>
+        <Text
+          style={{
+            padding: 40,
+            fontSize: 30,
+            color: 'black',
+            textAlign: 'center',
+          }}>
+          Little Lemon
+        </Text>
+      </View>
+    );
+  }
