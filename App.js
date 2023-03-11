172d23357060310d12a8370802a1dc2fe6282190
rajan <rajyotiroshan@gmail.com>
2023-03-11 11:56:19 +0530

    menuitem scrollview

--- App.js
+++ App.js
@@ -2,18 +2,22 @@ import { View } from "react-native";
 import LittleLemonHeader from "./components/LittleLemonHeader";
 import LittleLemonFooter from "./components/LittleLemonFooter";
 import WelcomeScreen from "./components/WelcomeScreen";
+import MenuItem from "./components/MenuItem";
+
 export default function App() {
   return (
     <>
       <View
         style={{
-          
+          flex: 1,
+          backgroundColor: '#495E57',
         }}
       >
         <LittleLemonHeader />
+        <MenuItem/>
       </View>
 
-      <View style={{
+{/*       <View style={{
           flex: 1,
           backgroundColor: "#495E57",
         }}>
@@ -22,7 +26,7 @@ export default function App() {
 
       <View style={{ backgroundColor: "#495E57" }}>
         <LittleLemonFooter />
-      </View>
+      </View> */}
     </>
   );
 }
